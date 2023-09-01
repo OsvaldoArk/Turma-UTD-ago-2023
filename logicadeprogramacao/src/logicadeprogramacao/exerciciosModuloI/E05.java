a1c72b4a30f56a0327467e352d70883f5aec4d55
Osvaldo Souza <osvaldo.ark@gmail.com>
2023-09-01 19:13:27 -0300

    feat: complementando o algoritmo de geração de números

--- logicadeprogramacao/src/logicadeprogramacao/exerciciosModuloI/E05.java
+++ logicadeprogramacao/src/logicadeprogramacao/exerciciosModuloI/E05.java
@@ -3,8 +3,23 @@ package logicadeprogramacao.exerciciosModuloI;
 public class E05 {
 	//gerando um número randomico
 	public static void main(String[] args) {
-		int numero = (int) Math.random()*100;
+		double numero1 = Math.random()*59+1;
 		
-		System.out.println(numero);
+		double numero2 = Math.random()*59+1;
+		
+		double numero3 = Math.random()*59+1;
+		
+		double numero4 = Math.random()*59+1;
+		
+		double numero5 = Math.random()*59+1;
+		
+		double numero6 = Math.random()*59+1;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(numero4);
+		System.out.println(numero5);
+		System.out.println(numero6);
 	}
 }
